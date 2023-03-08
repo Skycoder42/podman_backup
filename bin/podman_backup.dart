549f860af22c5a3d864df3f0fb454203e70a8de8
Skycoder42 <Skycoder42@users.noreply.github.com>
2023-03-08 17:24:12 +0100

    cleanup

--- bin/podman_backup.dart
+++ bin/podman_backup.dart
@@ -1,5 +1,1 @@
-import 'package:podman_backup/podman_backup.dart' as podman_backup;
-
-void main(List<String> arguments) {
-  print('Hello world: ${podman_backup.calculate()}!');
-}
+void main(List<String> arguments) {}
--- test/podman_backup_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:podman_backup/podman_backup.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
