03554099ab5d32a45478446fd97e48e8579768ab
Skycoder42 <Skycoder42@users.noreply.github.com>
2023-03-08 17:16:43 +0100

    create dart project

---
+++ bin/podman_backup.dart
@@ -0,0 +1,5 @@
+import 'package:podman_backup/podman_backup.dart' as podman_backup;
+
+void main(List<String> arguments) {
+  print('Hello world: ${podman_backup.calculate()}!');
+}
---
+++ test/podman_backup_test.dart
@@ -0,0 +1,8 @@
+import 'package:podman_backup/podman_backup.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
