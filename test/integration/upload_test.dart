669d9a8182fd295f9f6ed86fcdfaeb8edcbd3447
Skycoder42 <Skycoder42@users.noreply.github.com>
2023-03-17 07:08:59 +0100

    test upload

--- test/integration/upload_test.dart
+++ test/integration/upload_test.dart
@@ -1,3 +1,8 @@
+import 'dart:io';
+
+import 'package:podman_backup/src/cli/options.dart';
+import 'package:test/test.dart';
+
 import 'integration_test_case.dart';
 
 void main() => UploadTestCase().run();
@@ -6,8 +11,32 @@ class UploadTestCase extends IntegrationTestCase {
   @override
   String get name => 'upload';
 
+  Future<void> runSut() => runPodmanBackup(
+        backupMode: BackupMode.uploadOnly,
+        backupDir: backupDir,
+        cacheDir: cacheDir,
+      );
+
   @override
   void build() {
-    // TODO: implement build
+    test('Can upload a single backup', () async {
+      // arrange
+      final backupFile = await _createBackupFile('backup.tar.xz');
+
+      // act
+      await runSut();
+
+      // assert
+      expect(backupFile.existsSync(), isFalse);
+      final backedUpFile = _getBackedUpFile(backupFile);
+      expect(backedUpFile.existsSync(), isTrue);
+      expect(backedUpFile.readAsStringSync(), 'backup.tar.xz');
+    });
   }
+
+  Future<File> _createBackupFile(String backupFile) =>
+      File.fromUri(cacheDir.uri.resolve(backupFile)).writeAsString(backupFile);
+
+  File _getBackedUpFile(File backupFile) =>
+      File.fromUri(cacheDir.uri.resolve(backupFile.uri.pathSegments.last));
 }
